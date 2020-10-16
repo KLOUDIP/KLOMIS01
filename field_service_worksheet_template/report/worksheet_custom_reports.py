4a3a1b2d2201f3220be6528c7aa15539295efd02
nisusranga <ranga@nisus.lk>
2020-10-16 10:24:32 +0530

    [FIX]worksheet download issue

--- field_service_worksheet_template/report/worksheet_custom_reports.py
+++ field_service_worksheet_template/report/worksheet_custom_reports.py
@@ -13,9 +13,10 @@ class TaskCustomReport(models.AbstractModel):
         docs = self.env['worksheet.template.line'].browse(docids).sudo()
 
         worksheet_map = {}
+        task_sale_line_ids = []
         for task in docs.project_task_id:
             if task.worksheet_template_id:
-                task._reflect_timesheet_quantities()
+                task_sale_line_ids.append(task.sale_line_id)
         x_model = docs.template_id.model_id.model
         worksheet = self.env[x_model].search([('x_studio_line_id', '=', docs.id)], limit=1, order="create_date DESC")
         worksheet_map[docs.id] = worksheet
@@ -25,4 +26,5 @@ class TaskCustomReport(models.AbstractModel):
             'doc_model': 'worksheet.template.line',
             'docs': docs,
             'worksheet_map': worksheet_map,
+            'task_sale_line_ids': task_sale_line_ids
         }
