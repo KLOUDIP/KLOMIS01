d54de13ea712bfb48a7c1db707a46019a387949e
nisusranga <ranga@nisus.lk>
2020-10-22 14:10:12 +0530

    [Add]Initial commit payhere module

---
+++ payment_payhere_nisus/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Developed by Janath
+
+{
+    'name': 'Payhere Payment Acquirer',
+    'category': 'Payment Gateway',
+    'summary': 'Payment Acquirer: Payhere Implementation',
+    'version': '1.0',
+	'website': 'http://www.nisus.lk',
+    'author': 'Nisus Solutions(pvt) Ltd.',
+    'description': """Payhere (ePay) Acquirer""",
+    'depends': ['payment'],
+    'data': [
+        'views/payhere.xml',
+        'views/payment_acquirer.xml',
+        'data/payhere.xml',
+    ],
+	'license': 'Other proprietary',
+}
