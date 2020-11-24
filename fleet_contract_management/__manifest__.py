9d3eb1342e372370356bac5a9bb162b279c762aa
Ranga Dharmapriya <ranga@infroot.lk>
2020-11-24 17:05:22 +0530

    [ADD]link fleet contract with worksheet

--- fleet_contract_management/__manifest__.py
+++ fleet_contract_management/__manifest__.py
@@ -4,7 +4,7 @@
     "summary": "This module allow to add billing contact to contract view",
     "description": "This module allow to add billing contact to contract view",
     'category': 'fleet',
-    'version': '1.0.0',
+    'version': '1.0.1',
     "author": "Nisus Solutions (Pvt) Ltd",
     "website": "https://nisus.lk/",
     'data': [
--- fleet_contract_management/models/fleet_vehicle_cost.py
+++ fleet_contract_management/models/fleet_vehicle_cost.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
-from odoo import fields, models, _
+from odoo import fields, models, _, api
 
 class FleetVehicleLogContract(models.Model):
     _inherit = 'fleet.vehicle.log.contract'
 
     partner_id = fields.Many2one('res.partner', string="Billing Contract")
     is_activated = fields.Boolean(string="Activated")
+
+    @api.onchange('purchaser_id')
+    def _onchange_purchaser(self):
+        partners = []
+        if self.purchaser_id:
+            partners = self.purchaser_id.parent_id.child_ids.ids if self.purchaser_id.parent_id else self.purchaser_id.child_ids.ids
+        return {'domain': {'partner_id': [('id', 'in', partners)]}}
+
+
