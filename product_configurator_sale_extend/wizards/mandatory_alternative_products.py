94da85cb8308847a0c4ff1847af7f94c271a25b8
nisusranga <ranga@nisus.lk>
2021-03-30 12:46:07 +0530

    [FIX]Fixed compusory products selection issue

--- product_configurator_sale_extend/wizards/mandatory_alternative_products.py
+++ product_configurator_sale_extend/wizards/mandatory_alternative_products.py
@@ -15,15 +15,15 @@ class MandatoryAlternativeProductsWizard(models.TransientModel):
                                            context={'default_type': 'alternative'}, domain=[('type', '=', 'alternative')])
     mandatory_product_ids = fields.Many2many('product.product', 'mandatory_products', compute='_compute_mandatory_products', string='Mandatory Products')
     alternative_product_ids = fields.Many2many('product.product', 'alternative_products', compute='_compute_alternative_products', string='Alternative Products')
-    mandatory_products_available = fields.Boolean('Mandatory Products Available', compute='_check_products_availability')
-    alternative_products_available = fields.Boolean('Alternative Products Available', compute='_check_products_availability')
+    mandatory_products_available = fields.Boolean('Mandatory Products Available')
+    alternative_products_available = fields.Boolean('Alternative Products Available')
 
     @api.depends('mandatory_products.product_id')
     def _compute_mandatory_products(self):
         """Update field value - For domain purposes"""
         products = False
         if 'mandatory_products' in self.env.context:
-            products = self.env['product.product'].browse(self.env.context['mandatory_products']).filtered(lambda x: x.id not in self.mapped('mandatory_products').product_id.ids)
+            products = self.env['product.product'].browse(self.env.context['mandatory_products']).filtered(lambda x: x.product_tmpl_id.id not in self.mapped('mandatory_products').product_id.product_tmpl_id.ids)
         for rec in self:
             rec['mandatory_product_ids'] = products
 
@@ -32,22 +32,15 @@ class MandatoryAlternativeProductsWizard(models.TransientModel):
         """Update field value - For domain purposes"""
         products = False
         if 'alternative_products' in self.env.context:
-            products = self.env['product.product'].browse(self.env.context['alternative_products']).filtered(lambda x: x.id not in self.mapped('alternative_products').product_id.ids)
+            products = self.env['product.product'].browse(self.env.context['alternative_products']).filtered(lambda x: x.product_tmpl_id.id not in self.mapped('alternative_products').product_id.product_tmpl_id.ids)
         for rec in self:
             rec['alternative_product_ids'] = products
-
-    def _check_products_availability(self):
-        if 'state_list' in self.env.context:
-            self.update({
-                'mandatory_products_available': True if 'mandatory' in self.env.context['state_list'] else False,
-                'alternative_products_available': True if 'alternative' in self.env.context['state_list'] else False
-            })
 
     def action_confirm(self):
         """Extend action confirm with previously passed context values"""
         missing_products = False
         if 'mandatory_products' in self.env.context:
-            missing_products = list(set(self.env.context['mandatory_products']) - set(self.mandatory_products.mapped('product_id').ids))
+            missing_products = list(set(self.env['product.product'].browse(self.env.context['mandatory_products']).product_tmpl_id.ids) - set(self.mandatory_products.mapped('product_id').product_tmpl_id.ids))
         if missing_products:
             raise UserError(_('You need to add all Mandatory Products for proceed.'))
         else:
