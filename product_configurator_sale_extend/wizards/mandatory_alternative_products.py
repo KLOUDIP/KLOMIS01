cc3e968090d204dcd48e5c5bc3839f24fb1d01d3
nisusranga <ranga@nisus.lk>
2021-03-30 17:39:46 +0530

    [IMP]down-grade version to 1.0.4

--- product_configurator_sale_extend/wizards/mandatory_alternative_products.py
+++ product_configurator_sale_extend/wizards/mandatory_alternative_products.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from lxml import etree
 
 from odoo import models, fields, tools, api, _
 from odoo.exceptions import UserError
@@ -14,15 +15,15 @@ class MandatoryAlternativeProductsWizard(models.TransientModel):
                                            context={'default_type': 'alternative'}, domain=[('type', '=', 'alternative')])
     mandatory_product_ids = fields.Many2many('product.product', 'mandatory_products', compute='_compute_mandatory_products', string='Mandatory Products')
     alternative_product_ids = fields.Many2many('product.product', 'alternative_products', compute='_compute_alternative_products', string='Alternative Products')
-    mandatory_products_available = fields.Boolean('Mandatory Products Available')
-    alternative_products_available = fields.Boolean('Alternative Products Available')
+    mandatory_products_available = fields.Boolean('Mandatory Products Available', compute='_check_products_availability')
+    alternative_products_available = fields.Boolean('Alternative Products Available', compute='_check_products_availability')
 
     @api.depends('mandatory_products.product_id')
     def _compute_mandatory_products(self):
         """Update field value - For domain purposes"""
         products = False
         if 'mandatory_products' in self.env.context:
-            products = self.env['product.product'].browse(self.env.context['mandatory_products']).filtered(lambda x: x.product_tmpl_id.id not in self.mapped('mandatory_products').product_id.product_tmpl_id.ids)
+            products = self.env['product.product'].browse(self.env.context['mandatory_products']).filtered(lambda x: x.id not in self.mapped('mandatory_products').product_id.ids)
         for rec in self:
             rec['mandatory_product_ids'] = products
 
@@ -31,15 +32,22 @@ class MandatoryAlternativeProductsWizard(models.TransientModel):
         """Update field value - For domain purposes"""
         products = False
         if 'alternative_products' in self.env.context:
-            products = self.env['product.product'].browse(self.env.context['alternative_products']).filtered(lambda x: x.product_tmpl_id.id not in self.mapped('alternative_products').product_id.product_tmpl_id.ids)
+            products = self.env['product.product'].browse(self.env.context['alternative_products']).filtered(lambda x: x.id not in self.mapped('alternative_products').product_id.ids)
         for rec in self:
             rec['alternative_product_ids'] = products
+
+    def _check_products_availability(self):
+        if 'state_list' in self.env.context:
+            self.update({
+                'mandatory_products_available': True if 'mandatory' in self.env.context['state_list'] else False,
+                'alternative_products_available': True if 'alternative' in self.env.context['state_list'] else False
+            })
 
     def action_confirm(self):
         """Extend action confirm with previously passed context values"""
         missing_products = False
         if 'mandatory_products' in self.env.context:
-            missing_products = list(set(self.env['product.product'].browse(self.env.context['mandatory_products']).product_tmpl_id.ids) - set(self.mandatory_products.mapped('product_id').product_tmpl_id.ids))
+            missing_products = list(set(self.env.context['mandatory_products']) - set(self.mandatory_products.mapped('product_id').ids))
         if missing_products:
             raise UserError(_('You need to add all Mandatory Products for proceed.'))
         else:
@@ -69,7 +77,7 @@ class MandatoryAlternativeProductsSelect(models.TransientModel):
     _name = "mandatory.alternative.products.select"
     _description = 'Mandatory Alternative Products Select'
 
-    product_id = fields.Many2one('product.product', string='Product')
-    quantity = fields.Integer(string='Quantity', default=1)
+    product_id = fields.Many2one('product.product', string='Product', required=True)
+    quantity = fields.Integer(string='Quantity', required=True, default=1)
     mandatory_alternative_products_id = fields.Many2one('mandatory.alternative.products', string='Mandatory Alternative Products')
     type = fields.Selection([('mandatory', 'Mandatory'), ('alternative', 'Alternative')], string='Type')
