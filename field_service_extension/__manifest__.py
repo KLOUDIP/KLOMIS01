72be8da355206e0c1329f29e6a38832f9d5148e1
nisusranga <ranga@nisus.lk>
2021-06-07 19:34:28 +0530

    [INIT]Initial Commit field_service_extension

---
+++ field_service_extension/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'FieldService Extension',
+    "summary": "This module allow to add v13 features to v14",
+    "description": "This module allow to add v13 features to v14",
+    'category': 'Operations/Field Service',
+    'version': '1.0.0',
+    "author": "Nisus Solutions (Pvt) Ltd",
+    "website": "https://nisus.lk/",
+    'data': [
+        'views/project_views.xml',
+    ],
+    'depends': ['project', 'project_enterprise'],
+}
