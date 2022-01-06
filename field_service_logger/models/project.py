c6304abc4a3b0aa7577ab1daf44ecb2bc2e7e7c4
nisusranga <ranga@nisus.lk>
2022-01-06 17:17:47 +0530

    [ADD]Added field_service_logger module

---
+++ field_service_logger/models/project.py
@@ -0,0 +1,10 @@
+from odoo import models, fields, api, _
+
+class Task(models.Model):
+    _inherit = "project.task"
+
+    planned_date_begin = fields.Datetime(track_visibility='onchange')
+    partner_id = fields.Many2one(track_visibility='onchange')
+    partner_email = fields.Char(track_visibility='onchange')
+    partner_mobile = fields.Char(track_visibility='onchange')
+    partner_phone = fields.Char(track_visibility='onchange')
