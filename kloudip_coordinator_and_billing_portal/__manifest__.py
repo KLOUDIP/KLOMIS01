bfb84124d02e96a6308d4640490b2b8e54d22191
Sachitha Perera <sachitha@nisus.lk>
2023-04-07 17:34:46 +0530

    [IMP] Web Portal Coordination by and Billing by

---
+++ kloudip_coordinator_and_billing_portal/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+
+{
+    'name': 'KLOUDIP SO Coordinator And Billing Responsible',
+    'summary': "Coordinator and billing responsible for portal view",
+    'description': """
+            Add coordinator and billing responsible to portal view
+    """,
+    'version': '1.0.0',
+    'category': 'Sale',
+    'author': 'Nisus Solutions (Pvt) Ltd',
+    'website': 'http://www.nisus.lk',
+    'depends': [
+        'kloudip_so_coordinator_and_billing_responsible',
+        'portal'
+    ],
+    'data': [
+        'views/portal_templates.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
+# Actual Version 1.0.6
+
