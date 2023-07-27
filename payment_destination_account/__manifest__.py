1b8c7c8b8e2aadc68302b5e9b4b6b7548b2bc5bf
rangadharmapriya <ranga@infroot.com>
2023-07-27 13:38:52 +0530

    [ADD]Added payment_destination_account module

---
+++ payment_destination_account/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Payment Destination Account',
+    "summary": "This module allow to select destination account on payment screen",
+    "description": "This module allow to select destination account on payment screen",
+    'category': 'Account',
+    'version': '15.0.1.0.1',
+    "author": "BitbrainHub",
+    "email": "bitbrainhub@gmail.com",
+    'depends': [
+        'account'
+    ],
+    'data': [
+        'views/account_payment_view.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'licence': 'LGPL-3',
+}
