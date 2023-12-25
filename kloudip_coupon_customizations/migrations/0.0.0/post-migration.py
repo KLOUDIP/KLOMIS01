c83a922cef03a904df489c0272abe71b77769515
rangadharmapriya <ranga@infroot.com>
2023-12-26 00:44:08 +0530

    [IMP]Remove unwated migration files

--- kloudip_coupon_customizations/migrations/0.0.0/post-migration.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-def migrate(cr, version):
-    cr.execute("UPDATE loyalty_card b SET b.sale_order_id = a.sale_id, b.state = a.c_state FROM coupon_loyalty_temp a WHERE a.coupon_code = b.code")
-    cr.execute("DROP TABLE IF EXISTS coupon_loyalty_temp")
--- kloudip_coupon_customizations/migrations/0.0.0/pre-migration.py
+++ kloudip_coupon_customizations/migrations/0.0.0/pre-migration.py
@@ -1,5 +1,3 @@
 # -*- coding: utf-8 -*-
 def migrate(cr, version):
    cr.execute("UPDATE sale_order SET next_invoice_date='2023-03-27' WHERE next_invoice_date < start_date")
-   cr.execute("CREATE TABLE coupon_loyalty_temp (sale_id INT, coupon_id INT, coupon_code VARCHAR (255), c_state VARCHAR(255))")
-   cr.execute("INSERT INTO coupon_loyalty_temp (sale_id, coupon_id, coupon_code, c_state) SELECT 'sale_order_id', 'id', 'code', 'state' FROM coupon_coupon WHERE sale_order_id IS NOT NULL")
