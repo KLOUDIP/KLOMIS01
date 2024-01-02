dba9bc723bb1a05ed24ca2c015ca943f80252308
rangadharmapriya <ranga@infroot.com>
2024-01-02 21:01:59 +0530

    [FIX]Invoice contact block issue

--- contact_restriction/models/account_move.py
+++ contact_restriction/models/account_move.py
@@ -10,12 +10,13 @@ class AccountMove(models.Model):
     @api.model
     def create(self, values):
         partner = self.env['res.partner'].browse(values.get('partner_id'))
-        if partner and partner.is_blocked or partner.parent_id.is_blocked:
+        invoice_partner = self.env['res.partner'].browse(values.get('partner_invoice_id'))
+        if partner and partner.is_blocked or invoice_partner.is_blocked:
             raise UserError("This contact is blocked. You cannot create a Sales Order.")
         return super(AccountMove, self).create(values)
 
     def write(self, values):
         rec = super(AccountMove, self).write(values)
-        if self.partner_id.is_blocked or self.partner_id.parent_id.is_blocked:
+        if self.partner_id.is_blocked or self.partner_invoice_id.is_blocked:
             raise UserError("This contact is blocked.")
         return rec
--- contact_restriction/models/sale_order.py
+++ contact_restriction/models/sale_order.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models, api
+from odoo import models, api
 from odoo.exceptions import UserError
 
 class SaleOrder(models.Model):
@@ -9,12 +9,13 @@ class SaleOrder(models.Model):
     @api.model
     def create(self, values):
         partner = self.env['res.partner'].browse(values.get('partner_id'))
-        if partner and partner.is_blocked or partner.parent_id.is_blocked:
+        invoice_partner = self.env['res.partner'].browse(values.get('partner_invoice_id'))
+        if partner and partner.is_blocked or invoice_partner.is_blocked:
             raise UserError("This contact is blocked. You cannot create a Sales Order.")
         return super(SaleOrder, self).create(values)
 
     def write(self, values):
         rec = super(SaleOrder, self).write(values)
-        if self.partner_id.is_blocked or self.partner_id.parent_id.is_blocked:
+        if self.partner_id.is_blocked or self.partner_invoice_id.is_blocked:
             raise UserError("This contact is blocked.")
         return rec
