e044b4cd69ec403931ebd4fe33e13f7afae2689a
rangadharmapriya <ranga@infroot.com>
2024-01-02 23:05:50 +0530

    [FIX]Fixed invoice creation issue

--- contact_restriction/models/account_move.py
+++ contact_restriction/models/account_move.py
@@ -10,13 +10,12 @@ class AccountMove(models.Model):
     @api.model
     def create(self, values):
         partner = self.env['res.partner'].browse(values.get('partner_id'))
-        invoice_partner = self.env['res.partner'].browse(values.get('partner_invoice_id'))
-        if partner and partner.is_blocked or invoice_partner.is_blocked:
+        if partner and partner.is_blocked:
             raise UserError("This contact is blocked. You cannot create a Sales Order.")
         return super(AccountMove, self).create(values)
 
     def write(self, values):
         rec = super(AccountMove, self).write(values)
-        if self.partner_id.is_blocked or self.partner_invoice_id.is_blocked:
+        if self.partner_id.is_blocked:
             raise UserError("This contact is blocked.")
         return rec
--- contact_restriction/models/res_partner.py
+++ contact_restriction/models/res_partner.py
@@ -6,7 +6,7 @@ from odoo import fields, models
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    is_blocked = fields.Boolean(string="Is Blocked", default=False)
+    is_blocked = fields.Boolean(string="Is Blocked", default=False, tracking=True)
 
     def action_block_contact(self):
         self.write({'is_blocked': True})
