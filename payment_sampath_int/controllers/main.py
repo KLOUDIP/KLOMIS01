d0c4d16b0d076a17b1631e17c8576151e1812b36
rangadharmapriya <ranga@infroot.com>
2024-01-11 14:39:05 +0530

    [ADD]Added payment_Sampath_init module

---
+++ payment_sampath_int/controllers/main.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+import logging
+import pprint
+from datetime import datetime
+
+
+from odoo import http, SUPERUSER_ID
+from odoo.http import request
+
+_logger = logging.getLogger(__name__)
+
+
+class SampathIntController(http.Controller):
+    _return_url = '/payment/sampath_int/return'
+    _webhook_url = '/payment/sampath_int/webhook'
+
+    def _confirm_sampath_int_transaction(self, payment_response):
+        """
+        @private - confirm the transaction with response data
+        """
+        provider_id = request.env['payment.provider'].with_user(SUPERUSER_ID).search([('code', '=', 'sampath_int')], limit=1)
+        tx_sudo = request.env['payment.transaction'].sudo()._get_tx_from_notification_data('sampath_int', payment_response)
+
+        if tx_sudo:
+            uuid = tx_sudo.provider_reference
+        else:
+            uuid = ''
+
+        values = {
+            "version": "1.5",
+            "operation": "PAYMENT_COMPLETE",
+            "msgId": uuid,
+            "requestDate": datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f%z"),
+            "validateOnly": False,
+            "requestData": {
+                "clientId": provider_id.sampath_client_id,
+                "reqid": payment_response.get('reqid')
+            }
+        }
+
+        confirm_response = provider_id._sampath_make_request(values)
+        return confirm_response
+
+    @http.route(_return_url, type='http', auth='public', methods=['POST', 'GET'], csrf=False, save_session=False)
+    def sampath_int_return_from_checkout(self, **raw_data):
+        """ Process the notification data sent by Sampath after redirection from checkout.
+
+        The route is flagged with `save_session=False` to prevent Odoo from assigning a new session
+        to the user if they are redirected to this route with a POST request. Indeed, as the session
+        cookie is created without a `SameSite` attribute, some browsers that don't implement the
+        recommended default `SameSite=Lax` behavior will not include the cookie in the redirection
+        request from the payment provider to Odoo. As the redirection to the '/payment/status' page
+        will satisfy any specification of the `SameSite` attribute, the session of the user will be
+        retrieved and with it the transaction which will be immediately post-processed.
+
+        :param dict raw_data: The un-formatted notification data
+        """
+        tx_sudo = request.env['payment.transaction'].sudo()._get_tx_from_notification_data('sampath_int', raw_data)
+        _logger.info("handling redirection from Sampath with data:\n%s", pprint.pformat(raw_data))
+
+        confirm_response = self._confirm_sampath_int_transaction(raw_data)
+        _logger.info("Processing transaction with data:\n%s", pprint.pformat(confirm_response))
+
+        tx_sudo._handle_notification_data('sampath_int', confirm_response)
+        return request.redirect('/payment/status')
---
+++ payment_sampath_int/models/payment_provider.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+import logging
+import requests
+import pprint
+import hmac
+import hashlib
+import json
+import uuid
+
+
+from odoo import fields, models, _, api
+from odoo.exceptions import ValidationError
+
+_logger = logging.getLogger(__name__)
+
+
+class PaymentProvider(models.Model):
+    _inherit = 'payment.provider'
+
+    code = fields.Selection(
+        selection_add=[('sampath_int', "Sampath_int Bank International")], ondelete={'sampath_int': 'set default'})
+    sampath_int_client_id = fields.Char(
+        string="Sampath ClientID", required_if_provider='sampath_int', groups='base.group_system')
+    sampath_int_hmac_secret = fields.Char(string="Sampath HMAC Secret", groups='base.group_system')
+    sampath_int_auth_token = fields.Char(
+        string="Auth Token", help="Auth token for confirm the transaction", required_if_provider='sampath_int')
+
+    def _sampath_int_get_api_url(self):
+        """ sampath URLS """
+
+        if self.state == 'enabled':
+            return 'https://sampath.paycorp.lk/rest/service/proxy'
+        else:
+            return 'https://sampath.paycorp.lk/rest/service/proxy'
+
+    def _sampath_make_request(self, payload=None):
+        """ Make a request to Sampath API at the specified endpoint.
+
+        Note: self.ensure_one()
+
+        :param str endpoint: The endpoint to be reached by the request.
+        :param dict payload: The payload of the request.
+        :param str method: The HTTP method of the request.
+        :return The JSON-formatted content of the response.
+        :rtype: dict
+        :raise ValidationError: If an HTTP error occurs.
+        """
+        self.ensure_one()
+
+        url = self._sampath_int_get_api_url()
+
+        hmac_secret = self._sampath_generate_hmac(payload)
+
+        headers = {
+            'AUTHTOKEN': f'{self.sampath_int_auth_token}',
+            'HMAC': f'{hmac_secret}',
+            'Content-Type': 'application/json'
+        }
+
+        try:
+            response = requests.post(url, json=payload, headers=headers, timeout=10)
+            try:
+                response.raise_for_status()
+            except requests.exceptions.HTTPError:
+                _logger.exception(
+                    "Invalid API request at %s with data:\n%s", url, pprint.pformat(payload),
+                )
+                response_content = response.json()
+                error_code = response_content.get('error')
+                error_message = response_content.get('message')
+                raise ValidationError("Sampath: " + _(
+                    "The communication with the API failed"
+                    "information: '%s' (code %s)", error_message, error_code
+                ))
+        except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
+            _logger.exception("Unable to reach endpoint at %s", url)
+            raise ValidationError(
+                "Sampath: " + _("Could not establish the connection to the API.")
+            )
+        return response
+
+    def _sampath_generate_hmac(self, payload):
+        HMACSecret = self.sampath_int_hmac_secret
+        raw_payload = json.dumps(payload)
+        hmac_object = hmac.new(key=HMACSecret.encode(), msg=raw_payload.encode(), digestmod=hashlib.sha256)
+
+        HMAC = hmac_object.hexdigest()
+
+        return HMAC
