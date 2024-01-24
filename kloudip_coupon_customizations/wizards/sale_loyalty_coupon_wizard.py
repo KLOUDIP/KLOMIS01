7b509aab32c2382bb35af88aa8e7807d5b2693d2
rangadharmapriya <ranga@infroot.com>
2024-01-24 16:26:21 +0530

    [IMP]Filter coupons based on the poroduct amount

--- kloudip_coupon_customizations/wizards/sale_loyalty_coupon_wizard.py
+++ kloudip_coupon_customizations/wizards/sale_loyalty_coupon_wizard.py
@@ -8,7 +8,18 @@ class SaleLoyaltyCouponWizard(models.TransientModel):
 
     coupon_id = fields.Many2one('loyalty.card', string='Coupon')
     partner_id = fields.Many2one('res.partner', string='Partner')
+    reward_product_ids = fields.Many2many("product.product", string="Reward Product", compute="compute_reward_product")
 
     @api.onchange('coupon_id')
     def onchange_coupon_id(self):
         self.update({'coupon_code': self.coupon_id.code})
+
+    @api.depends('coupon_id')
+    def compute_reward_product(self):
+        reward = self.env['loyalty.reward']
+        for rec in self:
+            reward = reward.search([('discount_product_ids', 'in', rec.order_id.order_line.product_id.ids)])
+            if reward:
+                rec.reward_product_ids = [(6, 0, reward.discount_line_product_id.ids)]
+            else:
+                rec.reward_product_ids = False
