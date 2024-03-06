e88078c85377c8150cca6bbc621cc2ce47f658f8
devrangadu <devrangadu@gmail.com>
2024-03-07 01:05:08 +0530

    [FIX]Fixed adress not showing issue on field service task

--- field_service_logger/models/project.py
+++ field_service_logger/models/project.py
@@ -5,6 +5,5 @@ class Task(models.Model):
 
     planned_date_begin = fields.Datetime(track_visibility='onchange')
     partner_id = fields.Many2one(track_visibility='onchange')
-    partner_email = fields.Char(track_visibility='onchange')
     partner_mobile = fields.Char(track_visibility='onchange')
     partner_phone = fields.Char(track_visibility='onchange')
