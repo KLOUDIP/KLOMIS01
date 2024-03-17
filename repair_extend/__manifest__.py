d588faa22f0cfe9f7d79656eb9886203c1fd3f27
devrangadu <devrangadu@gmail.com>
2024-03-17 13:49:10 +0530

    [ADD]Added repair_extend module

---
+++ repair_extend/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Repair Extend',
+    "summary": "This module allow add new feature to repair module",
+    "description": "This module allow add new feature to repair module",
+    'category': 'Inventory/Inventory',
+    'version': '1.0.1',
+    "author": "BitbrainHub",
+    "email": "bitbrainhub@gmail.com",
+    'depends': [
+        'repair',
+    ],
+    'data': [
+        'views/sale_order_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'licence': 'LGPL-3',
+}
