1f526825f95effd940c6759cff2f65688fa91c32
devrangadu <devrangadu@gmail.com>
2024-03-19 21:08:22 +0530

    [IMP]Allow users to slect the repair order from delivery order

--- repair_extend/__manifest__.py
+++ repair_extend/__manifest__.py
@@ -4,15 +4,17 @@
     "summary": "This module allow add new feature to repair module",
     "description": "This module allow add new feature to repair module",
     'category': 'Inventory/Inventory',
-    'version': '1.0.2',
+    'version': '1.0.3',
     "author": "BitbrainHub",
     "email": "bitbrainhub@gmail.com",
     'depends': [
         'repair',
+        'stock'
     ],
     'data': [
         'views/sale_order_views.xml',
         'views/repair_views.xml',
+        'views/stock_picking_views.xml'
     ],
     'installable': True,
     'application': True,
