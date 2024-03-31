4f517ba547ee0fa6f1d5e0939354c3639ed3904a
devrangadu <devrangadu@gmail.com>
2024-04-01 02:30:36 +0530

    [ADD]Added user_extension_endpoint module

---
+++ user_extension_endpoint/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "User Extension Endpoint",
+    "author": "Ranga Dharmapriya",
+    "email": "rangadharmapriya@gmail.com",
+    "website": "",
+    "support": "",
+    "category": "MISC",
+    "summary": "Endpoint for get user extension",
+    "description": """
+Endpoint for get user extension
+    """,
+    "version": "17.0.1.0.0",
+    "depends": [
+        'base'
+    ],
+    "data": [],
+    'assets': {},
+    "application": False,
+    "auto_install": False,
+    "installable": True,
+    "license": 'LGPL-3'
+}
