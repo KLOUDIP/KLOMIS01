65d6eb2f462988fab2d24b40ade1900a2a5468ed
rangadu <rangadharmapriya@gmail.com>
2024-05-15 00:17:15 +0530

    [FIX]Disabled active unit calculation

--- so_assignees/models/active_units.py
+++ so_assignees/models/active_units.py
@@ -12,39 +12,39 @@ _logger = logging.getLogger(__name__)
 class ActiveUnits(models.Model):
     _inherit = 'active.units'
 
-    @api.model_create_multi
-    def create(self, values):
-        _logger.info("--------------------active.units - create--------------------")
-        _logger.info(values)
-        records = super(ActiveUnits, self).create(values)
-        for rec in records:
-            contracts = rec.contract_ids.filtered(lambda x: x.sale_id.coordinator_id.id == False)
-            if len(contracts) > 0:
-                rec.write({'contract_ids': [(3, contract.id) for contract in contracts]})
-            else:
-                contracts = rec.contract_ids.filtered(lambda x: x.sale_id.coordinator_id.id != False)
-                if len(contracts) > 0:
-                    self.update_monthly_rec(contracts[0].id)
-                    self.update_coordinator_unit_line(contracts[0].id, 'add')
-        return records
+    # @api.model_create_multi
+    # def create(self, values):
+    #     _logger.info("--------------------active.units - create--------------------")
+    #     _logger.info(values)
+    #     records = super(ActiveUnits, self).create(values)
+    #     for rec in records:
+    #         contracts = rec.contract_ids.filtered(lambda x: x.sale_id.coordinator_id.id == False)
+    #         if len(contracts) > 0:
+    #             rec.write({'contract_ids': [(3, contract.id) for contract in contracts]})
+    #         else:
+    #             contracts = rec.contract_ids.filtered(lambda x: x.sale_id.coordinator_id.id != False)
+    #             if len(contracts) > 0:
+    #                 self.update_monthly_rec(contracts[0].id)
+    #                 self.update_coordinator_unit_line(contracts[0].id, 'add')
+    #     return records
 
-    def write(self, vals):
-        _logger.info("--------------------active.units - write--------------------")
-        _logger.info(vals)
-        rec = super(ActiveUnits, self).write(vals)
-        if 'contract_ids' in vals:
-            for contract in vals.get('contract_ids', []):
-                if contract[0] == 4:
-                    self.update_monthly_rec(contract[1])
-                    self.update_coordinator_unit_line(contract[1], 'add')
-                elif contract[0] == 6:
-                    if len(contract[2]) > 0:
-                        self.update_monthly_rec(contract[2][0])
-                        self.update_coordinator_unit_line(contract[2][0], 'add')
-                else:
-                    self.unlink_monthly_rec(contract[1])
-                    self.update_coordinator_unit_line(contract[1], 'remove')
-        return rec
+    # def write(self, vals):
+    #     _logger.info("--------------------active.units - write--------------------")
+    #     _logger.info(vals)
+    #     rec = super(ActiveUnits, self).write(vals)
+    #     if 'contract_ids' in vals:
+    #         for contract in vals.get('contract_ids', []):
+    #             if contract[0] == 4:
+    #                 self.update_monthly_rec(contract[1])
+    #                 self.update_coordinator_unit_line(contract[1], 'add')
+    #             elif contract[0] == 6:
+    #                 if len(contract[2]) > 0:
+    #                     self.update_monthly_rec(contract[2][0])
+    #                     self.update_coordinator_unit_line(contract[2][0], 'add')
+    #             else:
+    #                 self.unlink_monthly_rec(contract[1])
+    #                 self.update_coordinator_unit_line(contract[1], 'remove')
+    #     return rec
 
     def update_monthly_rec(self, contract_id):
         res = self.env['active.units.monthly'].create({
