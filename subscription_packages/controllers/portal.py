a86426c582d3b215cec4b21eb807d173231866a8
rangadu <rangadharmapriya@gmail.com>
2024-06-18 22:55:32 +0530

    [IMP]Added subscription total section to portal view

--- subscription_packages/controllers/portal.py
+++ subscription_packages/controllers/portal.py
@@ -29,36 +29,3 @@ class CustomerPortal(portal.CustomerPortal):
         sale_id.with_user(request.env.user.id).message_post(body=f"Subscription plan changed by user {request.env.user.partner_id.name}", subject="Subscription plan changed")
         return _(
             f'Sale order: {sale_id.name}. Updated successfully using subscription package {subscription_pkg_id.display_name}.')
-
-    @route(['/get/sale/subscription/package/price'], type='json', auth="public", website=True)
-    def portal_get_sale_subscription_package_price(self, sale_id, subscription_package_id, product_id, recurring_line_id, **kwargs):
-        """
-        @public - get the package pricing with price-list
-        """
-        sale_id = request.env['sale.order'].with_user(SUPERUSER_ID).browse(int(sale_id))
-        subscription_plan_id = request.env['sale.subscription.plan'].with_user(SUPERUSER_ID).browse(
-            int(subscription_package_id))
-        product_id = request.env['product.product'].with_user(SUPERUSER_ID).browse(int(product_id))
-        plan_line_id = subscription_plan_id.product_subscription_pricing_ids.filtered(lambda x: x.product_template_id.id == product_id.product_tmpl_id.id and x.pricelist_id.id == sale_id.pricelist_id.id)
-        pricelist_id = plan_line_id.pricelist_id
-        recurring_line_id = request.env['sale.order.recurring'].with_user(SUPERUSER_ID).browse(int(recurring_line_id))
-
-        def _get_price(product_qty):
-            """
-            @nested - get the pricing with price-list
-            return: type - Tuple (format_amount, unit_price, price_subtotal)
-            """
-            # Compute product price
-            # product_price = pricelist_id._get_product_price(product_id, product_qty or 1.0, currency=pricelist_id.currency_id)
-            product_price = plan_line_id.price
-            product_price_formatted = format_amount(request.env, product_price, pricelist_id.currency_id) if pricelist_id else '0'
-            # Compute price subtotal
-            tax_results = request.env['account.tax']._compute_taxes([
-                recurring_line_id._convert_to_tax_base_line_dict()
-            ])
-            totals = list(tax_results['totals'].values())[0]
-            price_subtotal = totals['amount_untaxed']
-            price_subtotal_formatted = format_amount(request.env, price_subtotal, pricelist_id.currency_id) if pricelist_id else '0'
-            return tuple([product_price_formatted, product_price, price_subtotal_formatted, price_subtotal, bool(pricelist_id)])
-
-        return _get_price(recurring_line_id.quantity)
--- subscription_packages/static/src/js/sale_portal.js
+++ subscription_packages/static/src/js/sale_portal.js
@@ -1,7 +1,6 @@
 /** @odoo-module **/
 
 import publicWidget from "@web/legacy/js/public/public_widget";
-import { _t } from "@web/core/l10n/translation";
 
 publicWidget.registry.SaleSubscriptionPackage = publicWidget.Widget.extend({
     selector: '.subscription_packages',
@@ -18,29 +17,27 @@ publicWidget.registry.SaleSubscriptionPackage = publicWidget.Widget.extend({
         this.notification = this.bindService("notification");
     },
 
+    _refreshOrderUI(promises){
+        window.location.reload();
+    },
+
     /**
      * @private:
      * update the assigned sale order subscription package
      **/
     _updateSaleSubscriptionPackage(saleOrderId, subscriptionPriceId) {
         // Calling a rpc for update the sale order
-        const self = this;
-        this.rpc("/portal/update/sale/subscription/package", {
+        return this.rpc("/portal/update/sale/subscription/package", {
             'subscription_pkg_id': subscriptionPriceId,
             'sale_id': saleOrderId
-        }).then(function (message) {
-            // Add notification to notify the user
-            self.notification.add(_t(message), {
-                title: _t("Success"),
-                type: "success",
-            });
-        });
+        })
     },
 
     /**
      * @override: update necessary fields
      **/
-    _onChangeSubscriptionPackage(ev) {
+    async _onChangeSubscriptionPackage(ev) {
+        ev.preventDefault();
         const self = this;
         // Selection field styling
         let selectElement = document.getElementById('subscription-package');
@@ -53,43 +50,9 @@ publicWidget.registry.SaleSubscriptionPackage = publicWidget.Widget.extend({
         // Update prices of the products
         let saleId = $('.subscription_packages').attr('sale-id');
         let subscriptionPkgId = $('#subscription-package').val();
-        let pricelists = [];
-        let promises = [];
 
-        $('.subscription_packages > table > tbody > tr').each(function(index, tr) {
-            let $tr = $(tr);
-            let productId = $tr.data('productid');
-            let recurringLine = $tr.data('recurringline');
-
-            let promise = self.rpc("/get/sale/subscription/package/price", {
-                'sale_id': saleId,
-                'subscription_package_id': subscriptionPkgId,
-                'product_id': productId,
-                'recurring_line_id': recurringLine,
-            }).then(function (result) {
-                // price unit
-                let $unitPrice = $tr.find('.subscription-price-unit > strong > div');
-                $unitPrice.html('<span>' + result[0] + '</span>')
-                // subtotal
-                let $subtotal = $tr.find('#subscription_subtotal');
-                $subtotal.html('<span class="oe_order_line_price_subtotal">' + result[2] + '</span>')
-                // Add pricelist status to pricelists variable
-                pricelists.push(result[4])
-            });
-
-            promises.push(promise);
-        });
-
-        Promise.all(promises).then(function () {
-            // Check pricelist and visible/invisible the warning
-            if (pricelists.includes(false)) {
-                $('.pricelist-warning').removeClass('d-none');
-            } else {
-                $('.pricelist-warning').addClass('d-none');
-                // Update the subscription package to the sale order
-                self._updateSaleSubscriptionPackage(saleId, subscriptionPkgId);
-            }
-        });
+        let result = await self._updateSaleSubscriptionPackage(saleId, subscriptionPkgId);
+        self._refreshOrderUI(result);
     },
 
 })
