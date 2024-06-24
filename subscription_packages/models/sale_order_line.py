374e18dc69b9d9b174591e016332585d04f49bc7
rangadu <rangadharmapriya@gmail.com>
2024-06-25 02:36:55 +0530

    [FIX]Rental sale order quotation template error

--- subscription_packages/models/sale_order_line.py
+++ subscription_packages/models/sale_order_line.py
@@ -7,8 +7,7 @@ class SaleOrderLine(models.Model):
 
     @api.depends('order_id.rental_start_date', 'order_id.rental_return_date', 'is_rental')
     def _compute_name(self):
-        product_ids = self.order_id.sale_order_recurring_ids.mapped('product_id').ids
-        recurring_lines = self.filtered(lambda x: x.product_id.id in product_ids)
+        recurring_lines = self.filtered(lambda x: x.recurring_invoice)
         if recurring_lines:
             super(SaleOrderLine, self - recurring_lines)._compute_name()
         else:
